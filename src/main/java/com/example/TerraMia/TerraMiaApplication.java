80f1bcd09d8d8ef250f9aab16cf1a5687bf579b2
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-22 22:50:48 +0100

    created project

---
+++ src/main/java/com/example/TerraMia/TerraMiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TerraMia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraMiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraMiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TerraMia/TerraMiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TerraMia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraMiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
