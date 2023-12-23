ab1e505ac013704fd3cf794c197ea84b712fd392
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2023-12-23 09:27:00 +0100

    added auth service and controller

---
+++ src/main/java/com/example/TerraMia/auth/AuthController.java
@@ -0,0 +1,67 @@
+package com.example.TerraMia.auth;
+
+
+import com.example.TerraMia.User.User;
+import com.example.TerraMia.User.UserService;
+import com.example.TerraMia.exceptions.BadRequestException;
+import com.example.TerraMia.payloads.Token;
+import com.example.TerraMia.payloads.UserLoginDTO;
+import com.example.TerraMia.payloads.UserLoginSuccessDTO;
+import com.example.TerraMia.payloads.UserRegistrationDTO;
+import com.example.TerraMia.security.JWTTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private UserService utenteService;
+
+
+    @PostMapping("/login")
+    public UserLoginSuccessDTO login(@RequestBody UserLoginDTO body) throws Exception {
+
+        return new UserLoginSuccessDTO(authService.authenticateUser(body));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public User saveUser(@RequestBody @Validated UserRegistrationDTO body, BindingResult validation){
+        if(validation.hasErrors()){
+            throw new BadRequestException(validation.getAllErrors());
+        } else {
+            try {
+                return authService.registerUser(body);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @GetMapping("/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    public User verifyToken(@PathVariable String token){
+            return jwtTools.verifyToken(token);
+    }
+    @GetMapping("/refreshToken/{refreshToken}")
+    @ResponseStatus(HttpStatus.OK)
+    public Token verifyRefreshToken(@PathVariable String refreshToken){
+        return jwtTools.verifyRefreshToken(refreshToken);
+    }
+
+
+
+}
---
+++ src/main/java/com/example/TerraMia/auth/AuthService.java
@@ -0,0 +1,69 @@
+package com.example.TerraMia.auth;
+
+
+import com.example.TerraMia.User.User;
+import com.example.TerraMia.User.UserRepository;
+import com.example.TerraMia.User.UserService;
+import com.example.TerraMia.enums.Role;
+import com.example.TerraMia.exceptions.BadRequestException;
+import com.example.TerraMia.exceptions.UnauthorizedException;
+import com.example.TerraMia.security.JWTTools;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.example.TerraMia.payloads.Token;
+import com.example.TerraMia.payloads.UserLoginDTO;
+import com.example.TerraMia.payloads.UserLoginSuccessDTO;
+import com.example.TerraMia.payloads.UserRegistrationDTO;
+import java.io.IOException;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserService usersService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+
+    public Token authenticateUser(UserLoginDTO body) throws Exception {
+        // 1. Verifichiamo che l'email dell'utente sia nel db
+        User user = usersService.findByEmail(body.email());
+        // 2. In caso affermativo, verifichiamo se la password corrisponde a quella trovata nel db
+        if(bcrypt.matches(body.password(), user.getPassword()))  {
+            // 3. Se le credenziali sono OK --> Genero un JWT e lo restituisco
+            return jwtTools.createToken(user);
+        } else {
+            // 4. Se le credenziali NON sono OK --> 401
+            throw new UnauthorizedException("Credenziali non valide!");
+        }
+    }
+
+
+    public User registerUser(UserRegistrationDTO body) throws IOException {
+
+        // verifico se l'email è già utilizzata
+        userRepository.findByEmail(body.email()).ifPresent( user -> {
+            throw new BadRequestException("L'email " + user.getEmail() + " è già utilizzata!");
+        });
+        User newUser = new User();
+        newUser.setPassword(bcrypt.encode(body.password()));
+        newUser.setEmail(body.email());
+        newUser.setNome(body.nome());
+        newUser.setCognome(body.cognome());
+        newUser.setRuolo(Role.ADMIN);
+        userRepository.save(newUser);
+
+        return newUser;
+    }
+
+}
